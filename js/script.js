688b70247bc3560ec36f90e287f484e1b0b55829
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-04-02 16:14:20 +0000

    changed image

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle() {
+  // input
+  const baseA = parseFloat(
+    document.getElementById("base-A").value
+  )
+  const baseB = parseFloat(
+    document.getElementById("height-of-triangle").value
+  )
+  const height = parseFloat(
+    document.getElementById("height-of-triangle").value
+  )
+  // process (corrected formula)
+  const areaOfTriangle = (baseA * heightOfTriangle) / 2
+
+  // output
+  document.getElementById("area").innerHTML =
+    "Area is: " + areaOfTriangle + " cm²"
+}
